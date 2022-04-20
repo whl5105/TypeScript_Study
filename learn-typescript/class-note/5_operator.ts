698624aca234e2f89462e99c9e9aa0f6aa1e9cfd
whl5105 <whl5105@naver.com>
2022-04-20 20:36:50 +0900

    유니온타입과 인터섹션타입

--- learn-typescript/class-note/5_operator.ts
+++ learn-typescript/class-note/5_operator.ts
@@ -4,7 +4,7 @@
 // logMessage('hihi');
 // logMessage(100);
 
-let suinType : string | number | boolean;
+
 
 function logMessage(value: string | number){
   if(typeof value === 'number'){
@@ -20,3 +20,43 @@ logMessage(100);
 
 //유니온 타입이 장점 
 //특정 변수나 파라미터에 다양항 타입지정이 가능 
+
+interface Developer {
+  name: string;
+  skill: string;
+}
+interface Person {
+  name: string;
+  age: number;
+}
+function askSomeon(someone: Developer | Person){
+  someone.name ;
+}
+askSomeon({name: '수인', skill :'타입스크립트'});
+askSomeon({name: '수인이', age :28});
+/*
+  유니온 타입은 공통되는 속성만 접근할 수  있다.  
+  유니온 타입 적용시 어떤값이 들어올지 모르기 때문에 보장된 속성에 대해서만 접근할 수 있는것 
+*/
+
+
+let suinType : string | number | boolean; 
+let suinsType : string & number & boolean; 
+
+function askSomeon(someone: Developer & Person){
+  someone.name ;
+  someone.skill ;
+  someone.age ;
+}
+askSomeon({name: '수인', skill :'타입스크립트', age :28});
+/*
+  유니온 타입의경우 공통되지 않은 속성을 타입가드 처리를 해주어야하는 반면 
+  인터섹션의 경우  string & number & boolean 세가지의 타입을 모두 가지고 있기떄문에 접근이 가능하다 . 
+ */
+
+  /*
+  유니온 타입과 인터섹션 타입의 차이점
+  - 유니온 타입의  : 타입 추론(가드) 필요 , 넘기는 인자의 경우 선택가능 
+  - 인터섹션 타입  : 별도의 타입추론 불필요 , 모든 타입을 포함하는 인자를 호출 해야한다. 
+  
+  */
